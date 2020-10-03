623b27ec07e69d8c49f18c2602a0631229c62818
Segun <shegunolanitori@gmail.com>
2020-10-03 19:50:47 +0100

    fix: changed HOC to a Class Component, fixing a bug from function components

--- utils/ProtectedRouteV.tsx
+++ utils/ProtectedRouteV.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import Cookies from "js-cookie";
 
 export function ProtectRouteV(WrappedComponent: any) {
-  return function () {
-    if (Cookies && Cookies.get("role") !== "vendor") {
+  return class extends React.Component {
+    componentDidMount(): void {
       if (typeof window === "object") {
-        window.location.href = "/vendor/login";
+        if (Cookies.get("role") !== "vendor")
+          window.location.href = "/vendor/login";
       }
     }
 
-    return <WrappedComponent {...arguments} />;
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
   };
 }
