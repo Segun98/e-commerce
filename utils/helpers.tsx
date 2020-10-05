58668a6696c40b51b24617e93475fc7db7a22f26
Segun <shegunolanitori@gmail.com>
2020-10-05 01:40:19 +0100

    feat: added a truncate string helper

--- utils/helpers.tsx
+++ utils/helpers.tsx
@@ -2,3 +2,14 @@
 export function Commas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+export function truncate(str) {
+  if (!str) {
+    return str;
+  }
+  if (str.length > 60) {
+    return str.slice(0, 60) + "...";
+  } else {
+    return str;
+  }
+}
