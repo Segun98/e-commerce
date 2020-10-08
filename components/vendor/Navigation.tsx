dd82fb23fcf564c883ec28d8e3f5648e124ca6d5
Segun <shegunolanitori@gmail.com>
2020-10-08 12:51:43 +0100

    fix: fixed react development error of server and client text content mismatch

--- components/vendor/Navigation.tsx
+++ components/vendor/Navigation.tsx
@@ -30,71 +30,70 @@ export const Navigation = () => {
   };
 
   return (
-    <>
-      <div className={isOpen ? "vendor-menu open" : "vendor-menu"}>
-        <header>
-          <span>PartyStore</span>
-          <button onClick={() => setIsOpen(!isOpen)}>
-            {isOpen ? <Icon name="close" /> : <Icon name="arrow-right" />}
-          </button>
-        </header>
+    <div className={isOpen ? "vendor-menu open" : "vendor-menu"}>
+      <header>
+        <span>PartyStore</span>
+        <button onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? <Icon name="close" /> : <Icon name="arrow-right" />}
+        </button>
+      </header>
 
-        {/* SHOW THIS TO VENDORS */}
-        {role && role === "vendor" && (
-          <ul>
-            <li>
-              <Link href="/vendor/dashboard">
-                <a>Dashboard</a>
-              </Link>
-            </li>
-            <li>
-              <Link
-                href={`/store/${User && User.business_name_slug}`}
-                as={`/store/${User && User.business_name_slug}`}
-              >
-                <a>Store</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/vendor/orders">
-                <a>Orders</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/store/new-item">
-                <a>Add New Product</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/vendor/account">
-                <a>Account</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>Payout?</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>Contact Us</a>
-              </Link>
-            </li>
-            <div className="logout-btn">
-              <Button
-                style={{ color: "white", background: "var(--deepblue)" }}
-                width="100%"
-                borderRadius="none"
-                onClick={handleLogout}
-              >
-                Logout
-              </Button>
-            </div>
-          </ul>
-        )}
-
-        {/* SHOW THIS TO CUSTOMERS */}
-        {role !== "vendor" || !role ? (
+      {/* SHOW THIS TO VENDORS */}
+      <div>
+        {role === "vendor" ? (
+          <section>
+            <ul>
+              <li>
+                <Link href="/vendor/dashboard">
+                  <a>Dashboard</a>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={`/store/${User && User.business_name_slug}`}
+                  as={`/store/${User && User.business_name_slug}`}
+                >
+                  <a>Store</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/vendor/orders">
+                  <a>Orders</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/store/new-item">
+                  <a>Add New Product</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/vendor/account">
+                  <a>Account</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <a>Payout?</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <a>Contact Us</a>
+                </Link>
+              </li>
+              <div className="logout-btn">
+                <Button
+                  style={{ color: "white", background: "var(--deepblue)" }}
+                  width="100%"
+                  borderRadius="none"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </Button>
+              </div>
+            </ul>
+          </section>
+        ) : (
           <ul className="menu-for-customers">
             <li>
               <Link href="/">
@@ -147,8 +146,6 @@ export const Navigation = () => {
                 <a>Cakes</a>
               </Link>
             </li>
-            <hr />
-            {/* LOGIN BUTTON FOR VENDORS  */}
             <div className="login-btn">
               <Button
                 style={{ color: "white", background: "var(--deepblue)" }}
@@ -161,110 +158,109 @@ export const Navigation = () => {
               </Button>
             </div>
           </ul>
-        ) : (
-          ""
         )}
-        <button className="vendor-menu-btn" onClick={() => setIsOpen(!isOpen)}>
-          {isOpen ? (
-            <Icon name="close" />
-          ) : (
-            <img src="/menu.svg" alt="menu svg" />
-          )}
-        </button>
-        <style jsx>{`
+      </div>
+
+      <button className="vendor-menu-btn" onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? (
+          <Icon name="close" />
+        ) : (
+          <img src="/menu.svg" alt="menu svg" />
+        )}
+      </button>
+      <style jsx>{`
+        .vendor-menu {
+          background: var(--softblue);
+          width: 250px;
+          height: 100vh;
+          border-right: 0.6px solid var(--softgrey);
+          margin-left: -250px;
+          position: relative;
+          position: fixed;
+          z-index: 2;
+          transition: 0.5s ease;
+        }
+
+        .vendor-menu.open {
+          margin-left: 0px;
+          transition: 0.5s ease;
+        }
+
+        .vendor-menu ul {
+          display: flex;
+          flex-direction: column;
+        }
+
+        .vendor-menu header {
+          font-style: italic;
+          font-size: 1.2rem;
+          font-weight: bold;
+          color: white;
+          margin-bottom: 10px;
+          margin-left: 0;
+          border-bottom: 1px solid;
+          padding: 10px 15px;
+          background: var(--deepblue);
+          display: flex;
+          justify-content: space-between;
+        }
+        .vendor-menu ul li {
+          margin: 10px 0;
+          padding-left: 10px;
+          padding-bottom: 5px;
+          display: block;
+          border-bottom: 1px solid var(--lightblue);
+        }
+        .vendor-menu-btn {
+          position: absolute;
+          top: 0;
+          right: 0;
+          margin-right: -45px;
+        }
+        .vendor-menu-btn img {
+          width: 40px;
+        }
+
+        .logout-btn {
+          margin-top: 50px;
+        }
+
+        @media only screen and (min-width: 700px) {
           .vendor-menu {
-            background: var(--softblue);
-            width: 250px;
-            height: 100vh;
-            border-right: 0.6px solid var(--softgrey);
-            margin-left: -250px;
-            position: relative;
-            position: fixed;
-            z-index: 2;
-            transition: 0.5s ease;
-          }
-
-          .vendor-menu.open {
             margin-left: 0px;
-            transition: 0.5s ease;
+            width: 200px;
+            position: static;
+          }
+          .vendor-menu-btn {
+            display: none;
+          }
+        }
+
+        @media only screen and (min-width: 1200px) {
+          .vendor-menu {
+            width: 250px;
           }
 
-          .vendor-menu ul {
-            display: flex;
-            flex-direction: column;
-          }
-
-          .vendor-menu header {
-            font-style: italic;
-            font-size: 1.2rem;
-            font-weight: bold;
-            color: white;
-            margin-bottom: 10px;
-            margin-left: 0;
-            border-bottom: 1px solid;
-            padding: 10px 15px;
-            background: var(--deepblue);
-            display: flex;
-            justify-content: space-between;
-          }
           .vendor-menu ul li {
             margin: 10px 0;
             padding-left: 10px;
             padding-bottom: 5px;
-            display: block;
-            border-bottom: 1px solid var(--lightblue);
           }
-          .vendor-menu-btn {
-            position: absolute;
-            top: 0;
-            right: 0;
-            margin-right: -45px;
+          .menu-for-customers li {
+            padding-bottom: 5px;
+            font-size: 1.05rem;
           }
-          .vendor-menu-btn img {
-            width: 40px;
-          }
+        }
 
-          .logout-btn {
-            margin-top: 50px;
+        @media only screen and (min-width: 1800px) {
+          .vendor-menu ul li {
+            font-size: 1.2rem;
           }
-
-          @media only screen and (min-width: 700px) {
-            .vendor-menu {
-              margin-left: 0px;
-              width: 200px;
-              position: static;
-            }
-            .vendor-menu-btn {
-              display: none;
-            }
+          .menu-for-customers li {
+            font-size: 1.2rem;
           }
-
-          @media only screen and (min-width: 1200px) {
-            .vendor-menu {
-              width: 250px;
-            }
-
-            .vendor-menu ul li {
-              margin: 10px 0;
-              padding-left: 10px;
-              padding-bottom: 5px;
-            }
-            .menu-for-customers li {
-              padding-bottom: 5px;
-              font-size: 1.05rem;
-            }
-          }
-
-          @media only screen and (min-width: 1800px) {
-            .vendor-menu ul li {
-              font-size: 1.2rem;
-            }
-            .menu-for-customers li {
-              font-size: 1.2rem;
-            }
-          }
-        `}</style>
-      </div>
-    </>
+        }
+      `}</style>
+    </div>
   );
 };
