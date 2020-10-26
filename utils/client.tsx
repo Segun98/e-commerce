a1c149173860595bc914484f1a9ae84b9574a129
Segun <shegunolanitori@gmail.com>
2020-10-26 23:07:20 +0100

    security

--- utils/client.tsx
+++ utils/client.tsx
@@ -2,45 +2,44 @@ import { GraphQLClient } from "graphql-request";
 
 //REFRESHTOKEN LINK
 export let restLink = [
-  "http://localhost:4000/api/refreshtoken",
   "https://apipartystore.herokuapp.com/api/refreshtoken",
+  "http://localhost:4000/api/refreshtoken",
   "https://apipartystore.vercel.app/api/refreshtoken",
 ];
 
 //IMAGE UPLOAD LINK
 export let uploadLink = [
-  "http://localhost:4000/api/upload",
   "https://apipartystore.herokuapp.com/api/upload",
+  "http://localhost:4000/api/upload",
   "https://apipartystore.vercel.app/api/upload",
 ];
 
 //GOOGLE OUATH REST API ENDPOINTS
 export let oAuthLoginLink = [
-  "http://localhost:4000/api/oauth/login",
   "https://apipartystore.herokuapp.com/api/oauth/login",
+  "http://localhost:4000/api/oauth/login",
   "https://apipartystore.vercel.app/api/oauth/login",
 ];
 
 export let oAuthSignupLink = [
-  "http://localhost:4000/api/oauth/signup",
   "https://apipartystore.herokuapp.com/api/oauth/signup",
+  "http://localhost:4000/api/oauth/signup",
   "https://apipartystore.vercel.app/api/oauth/signup",
 ];
 
 export let logoutLink = [
-  "http://localhost:4000/api/logout",
   "https://apipartystore.herokuapp.com/api/logout",
+  "http://localhost:4000/api/logout",
   "https://apipartystore.vercel.app/api/logout",
 ];
 
 //GOOGLE CLIENT ID
-export let CLIENT_ID =
-  "649409125932-gr4408gcakrmumvia7ju9k83c0o72cv1.apps.googleusercontent.com";
+export let CLIENT_ID = "";
 
 //GRAPHQL API ENDPOINTS
 let links = [
-  "http://localhost:4000/graphql",
   "https://apipartystore.herokuapp.com/graphql",
+  "http://localhost:4000/graphql",
   "https://apipartystore.vercel.app/graphql",
 ];
 
