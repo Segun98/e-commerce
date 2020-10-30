e62eece7f6cf2b436f83654ee558c38be6175423
Segun <shegunolanitori@gmail.com>
2020-10-30 23:30:44 +0100

    feat: building vendor welcome page

--- pages/vendor/become-a-vendor.tsx
+++ pages/vendor/become-a-vendor.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Head from "next/head";
 import { Layout } from "./../../components/Layout";
 import { Text } from "@chakra-ui/core";
+import Link from "next/link";
 
 const BecomeAVendor = () => {
   return (
@@ -13,10 +14,20 @@ const BecomeAVendor = () => {
         <header>
           <div className="intro-image">
             {/* Large screen  */}
-            <h1>Partner With Us, Grow Your Business The Right Way!</h1>
+            <div className="intro-desktop-caption">
+              <h1>Partner With Us, Grow Your Business The Right Way!</h1>
+              <Link href="/vendor/register">
+                <a>Get Started</a>
+              </Link>
+            </div>
           </div>
           {/* Mobile screen  */}
-          <p>Partner With Us, Grow Your Business The Right Way!</p>
+          <div className="intro-mobile-caption">
+            <p>Partner With Us, Grow Your Business The Right Way!</p>
+            <Link href="/vendor/register">
+              <a>Get Started</a>
+            </Link>
+          </div>
         </header>
         <section className="content-1"></section>
       </div>
