4845375cbdfaea90469617b2574c7190c8591913
Segun <shegunolanitori@gmail.com>
2020-11-02 14:43:38 +0100

    feat: building become a vendor page, inlined background image

--- pages/vendor/become-a-vendor.tsx
+++ pages/vendor/become-a-vendor.tsx
@@ -12,7 +12,10 @@ const BecomeAVendor = () => {
       </Head>
       <div className="become-vendor">
         <header>
-          <div className="intro-image">
+          <div
+            className="intro-image"
+            style={{ backgroundImage: 'url("/slider/service.jpg")' }}
+          >
             {/* Large screen  */}
             <div className="intro-desktop-caption">
               <h3>Partner With Us, Grow Your Business The Right Way!</h3>
