f703a38797b758ab94beea3014b9b67264219d62
Segun <shegunolanitori@gmail.com>
2020-12-15 15:34:01 +0100

    Feat: Used absolute imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import "../styles/App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 import Router from "next/router";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
-import { TokenProvider } from "../Context/TokenProvider";
+import { TokenProvider } from "@/Context/TokenProvider";
 import React from "react";
-import { UserProvider } from "../Context/UserProvider";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { UserProvider } from "@/Context/UserProvider";
 import { Provider } from "react-redux";
 import store from "@/redux/store";
 import { GlobalLayout } from "@/components/GlobalLayout";
