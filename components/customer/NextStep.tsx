8ce84d3693fbc5d2fd422ba41a7b3aae1e411ad6
Segun <shegunolanitori@gmail.com>
2021-01-05 19:28:41 +0100

    Feat: Provision to create multiple orders at once

--- components/customer/NextStep.tsx
+++ components/customer/NextStep.tsx
@@ -1,13 +1,13 @@
+import { graphQLClient } from "@/utils/client";
 import { Button, useToast } from "@chakra-ui/core";
 import { useRouter } from "next/router";
 import React from "react";
-import { useToken } from "../../Context/TokenProvider";
-import { createOrder } from "../../graphql/customer";
-import { Cart, MutationCreateOrderArgs } from "../../Typescript/types";
-import { useMutation } from "../../utils/useMutation";
+import { useToken } from "@/Context/TokenProvider";
+import { createOrder } from "@/graphql/customer";
+import { Cart, MutationCreateOrderArgs } from "@/Typescript/types";
 
 interface Iprops {
-  cart: Cart;
+  cart: Cart[];
   address: string;
   phone: string;
   request: string;
@@ -24,86 +24,92 @@ export const NextStep: React.FC<Iprops> = ({
   const router = useRouter();
 
   async function handleOrder() {
-    let sub = cart.product.price * cart.quantity + 1000;
-    const variables: MutationCreateOrderArgs | any = {
-      name: cart.product.name,
-      price: cart.product.price,
-      quantity: cart.quantity,
-      delivery_fee: 1000,
-      //@ts-ignore
-      subtotal: parseInt(sub),
-      request,
-      customer_email: cart.cartCreator.email,
-      vendor_email: cart.product.creator.email,
-      customer_phone: cart.cartCreator.phone,
-      vendor_phone: cart.product.creator.phone,
-      customer_address: address,
-      business_address: cart.product.creator.business_address,
-      product_id: cart.product_id,
-      prod_creator_id: cart.prod_creator_id,
-    };
-    if (!address || !phone) {
-      toast({
-        title: "Address Details Cannot Be Empty",
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-        position: "top",
-      });
-      return;
+    //very important!
+    let orderId = Date.now();
+
+    for await (const c of cart) {
+      //price + delivery
+      let sub = c.product.price * c.quantity;
+
+      const variables: MutationCreateOrderArgs = {
+        order_id: String(orderId),
+        name: c.product.name,
+        price: c.product.price,
+        quantity: c.quantity,
+        //@ts-ignore
+        subtotal: parseInt(sub),
+        request,
+        customer_email: c.cartCreator.email,
+        vendor_email: c.product.creator.email,
+        customer_phone: c.cartCreator.phone,
+        vendor_phone: c.product.creator.phone,
+        customer_address: address,
+        business_address: c.product.creator.business_address,
+        product_id: c.product_id,
+        prod_creator_id: c.prod_creator_id,
+      };
+      if (!address || !phone) {
+        toast({
+          title: "Address Details Cannot Be Empty",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+        });
+        return;
+      }
+
+      //restricted customer can't place an order
+      if (c.cartCreator.pending === "true") {
+        toast({
+          title: "Sorry you cannot make this Order at this time",
+          description:
+            "Your account is currently restricted, please contact us",
+          status: "info",
+          duration: 7000,
+          isClosable: true,
+          position: "top",
+        });
+        return;
+      }
+
+      //check if product is out of stock or creator is offline
+
+      if (
+        c.product.creator.online === "false" ||
+        c.product.in_stock === "false" ||
+        c.product.available_qty < 1
+      ) {
+        toast({
+          title: "Sorry you cannot make this Order at this time",
+          description:
+            "The product is either out of stock or Vendor is unavailable. Sorry for the incovinience, do check back later",
+          status: "info",
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+        });
+        return;
+      }
+
+      try {
+        graphQLClient.setHeader("authorization", `bearer ${Token}`);
+        await graphQLClient.request(createOrder, variables);
+      } catch (err) {
+        toast({
+          title: "An error occurred.",
+          description: "Please check your internet connection and refresh.",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+        });
+        return;
+      }
     }
 
-    //restricted customer can't place an order
-    if (cart.cartCreator.pending === "true") {
-      toast({
-        title: "Sorry you cannot make this Order at this time",
-        description: "Your account is currently restricted, please contact us",
-        status: "info",
-        duration: 7000,
-        isClosable: true,
-        position: "top",
-      });
-      return;
-    }
-
-    //check if product is out of stock or creator is offline
-
-    if (
-      cart.product.creator.online === "false" ||
-      cart.product.in_stock === "false" ||
-      cart.product.available_qty < 1
-    ) {
-      toast({
-        title: "Sorry you cannot make this Order at this time",
-        description:
-          "The product is either out of stock or Vendor is unavailable. Sorry for the incovinience, do check back later",
-        status: "info",
-        duration: 5000,
-        isClosable: true,
-        position: "top",
-      });
-      return;
-    }
-    const { data, error } = await useMutation(createOrder, variables, Token);
-
-    if (data) {
-      //order id
-      const { id } = data.createOrder;
-      //set session to store cart id
-      window.sessionStorage.setItem("cart_id", cart.id);
-      //payment page
-      router.push(`/product/pay/${id}`).then(() => window.scrollTo(0, 0));
-    }
-    if (error) {
-      toast({
-        title: "An error occurred.",
-        description: "check your internet connection and refresh.",
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-        position: "top",
-      });
-    }
+    //Route to payment page after loop
+    router.push(`/product/pay/${orderId}`).then(() => window.scrollTo(0, 0));
   }
 
   return (
